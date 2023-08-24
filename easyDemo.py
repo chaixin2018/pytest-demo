0c0763c49d6a1bfc7b839dfd8a2fe48739f59162
chaixin2018 <46804384+chaixin2018@users.noreply.github.com>
2023-08-24 15:08:10 +0900

    Update and rename easyDemo to easyDemo.py

--- easyDemo.py
+++ easyDemo.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from time import sleep
 
 # 创建一个 Chrome WebDriver 实例
 driver = webdriver.Chrome()
 
 # 打开网页
 driver.get("https://www.example.com")
-
+sleep(20)
 # 获取页面标题并打印
 print("页面标题:", driver.title)
 

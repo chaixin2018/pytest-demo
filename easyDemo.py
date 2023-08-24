3dd5d0aefb579569f80eee0f52eecbd0eea6b39e
chaixin2018 <46804384+chaixin2018@users.noreply.github.com>
2023-08-24 17:18:28 +0900

    Update easyDemo.py

--- easyDemo.py
+++ easyDemo.py
@@ -1,14 +1,22 @@
-from selenium import webdriver
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# 创建一个 Chrome WebDriver 实例
-driver = webdriver.Chrome()
+# 创建 ChromeOptions 对象，启用无头模式
+chrome_options = Options()
+chrome_options.add_argument('--headless')
 
+# 指定 Chrome WebDriver 的路径
+# webdriver_path = '/usr/local/bin/chromedriver'  # 将路径替换为实际的 Chrome WebDriver 路径
+
+# 创建 Chrome WebDriver 实例
+# driver = webdriver.Chrome(executable_path=webdriver_path, options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 # 打开网页
 driver.get("https://www.example.com")
-sleep(20)
+sleep(5)
 # 获取页面标题并打印
 print("页面标题:", driver.title)
-print("11111")
+
 # 关闭浏览器
 driver.quit()
